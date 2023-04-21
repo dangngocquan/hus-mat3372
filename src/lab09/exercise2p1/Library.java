0f457a83ca58227cd3fe0b5e125f0128852752ff
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-04-21 23:42:12 +0700

    lab09

--- src/lab09/exercise2p1/Library.java
+++ src/lab09/exercise2p1/Library.java
@@ -13,6 +13,7 @@ public class Library {
         for (Rent rent : rents) {
             if (rent != null && rent.getDiffDays() > diffDays) {
                 longestRent = rent;
+                diffDays = rent.getDiffDays();
             }
         }
         return longestRent;
